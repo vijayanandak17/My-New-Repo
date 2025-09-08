56fa4cea0bc4c2eade16892ac48d68ac665fd908
vijayanandak17 <vijayanandak17@gmail.com>
2025-09-08 09:58:57 +0530

    Updated file sent to GITHUB

--- Selenium-demo/Selenium-practice.py
+++ Selenium-demo/Selenium-practice.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+# Mister Dom
 
 driver = webdriver.Chrome()
 
